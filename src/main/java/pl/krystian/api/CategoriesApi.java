f65f3ffdedab14c935142aa2045c06f8d2bf6167
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-03 02:51:52 +0200

    Added apis to categories

--- src/main/java/pl/krystian/api/CategoriesApi.java
+++ src/main/java/pl/krystian/api/CategoriesApi.java
@@ -3,6 +3,9 @@ package pl.krystian.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,22 +27,27 @@ public class CategoriesApi {
 		return operation.addCategory(category);
 	}
 	
+	@PatchMapping("/updateCategory")
 	public String editCategory(@RequestBody CategoryEntity category) {
 		return operation.editCategory(category);
 	}
 	
+	@DeleteMapping("/deleteCategory")
 	public String deleteCategory(@RequestBody CategoryEntity category) {
 		return operation.deleteCategory(category);
 	}
 	
+	@GetMapping("/getAllCategories")
 	public List<CategoryEntity> getAllCategories() {
 		return operation.getAllCategories();
 	}
 	
-	public CategoryEntity getCategoryById(@RequestBody Long id) {
+	@GetMapping("/getCategoryById")
+	public CategoryEntity getCategoryById(@RequestBody Long id) {		
 		return operation.getCategoryById(id);
 	}
 	
+	@GetMapping("/getCategoryByName")
 	public CategoryEntity getCategoryByName(@RequestBody String name) {
 		return operation.getCategoryByName(name);
 	}
