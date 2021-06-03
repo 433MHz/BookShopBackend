a530468c5e2e75722c0048229b6391d80dd24731
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-03 10:59:23 +0200

    Added object to hold id of book and category that need to be connected in database

---
+++ src/main/java/pl/krystian/api/BookCategoryIdHolder.java
@@ -0,0 +1,21 @@
+package pl.krystian.api;
+
+public class BookCategoryIdHolder {
+	
+	private Long bookID;
+	private Long categoryID;
+	
+	
+	public Long getBookID() {
+		return bookID;
+	}
+	public void setBookID(Long bookID) {
+		this.bookID = bookID;
+	}
+	public Long getCategoryID() {
+		return categoryID;
+	}
+	public void setCategoryID(Long categoryID) {
+		this.categoryID = categoryID;
+	}
+}
