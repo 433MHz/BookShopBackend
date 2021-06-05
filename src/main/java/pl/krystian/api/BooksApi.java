1042292a17fa26feb739cc65759825e4c80acae9
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-05 10:54:40 +0200

    Added CrossOrigin annotation inside apis

--- src/main/java/pl/krystian/api/BooksApi.java
+++ src/main/java/pl/krystian/api/BooksApi.java
@@ -3,6 +3,7 @@ package pl.krystian.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,6 +17,7 @@ import pl.krystian.entities.BookEntity;
 
 @RestController
 @RequestMapping(path = "/books")
+@CrossOrigin
 public class BooksApi {
 	
 	@Autowired
